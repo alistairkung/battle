91f8aaf99afabd215bbcca2a33da2e1c97f1f8d6
Alistair Kung <alistaircckung@gmail.com>
2017-06-22 00:44:31 +0100

    added name form feature

--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,6 +0,0 @@
-describe "Testing Infrastructure", :type => :feature do
-  it "returns Testing infrastructure working!" do
-    visit ('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-end
