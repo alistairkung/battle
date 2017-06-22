31eaa523e1f51d9d7700ffc5b1da08af75400513
Alistair Kung <alistaircckung@gmail.com>
2017-06-22 12:47:12 +0100

    added feedback message upon attack

--- app.rb
+++ app.rb
@@ -11,11 +11,13 @@ class Battle < Sinatra::Base
     session[:player_1] = params[:player_1]
     session[:player_2] = params[:player_2]
     session[:health_2] = 100
+    session[:attack] = false
     redirect '/play'
   end
 
   post '/attack' do
     session[:health_2] -= 10
+    session[:attack] = true
     redirect '/play'
   end
 
@@ -23,6 +25,7 @@ class Battle < Sinatra::Base
     @player_1 = session[:player_1]
     @player_2 = session[:player_2]
     @health_2 = session[:health_2]
+    @attack = session[:attack]
     erb(:play)
   end
 
--- spec/features/hit_bar_spec.rb
+++ spec/features/hit_bar_spec.rb
@@ -4,9 +4,16 @@ feature "player health" do
     expect(page).to have_css('#p2_health', :text => '100')
   end
 
+  scenario "player clicks attack button" do
+    sign_in_and_play
+    click_on 'Attack'
+    expect(page).to have_content('You attacked player2!')
+  end
+
   scenario "player 2 is attacked" do
     sign_in_and_play
     click_on 'Attack'
     expect(page).to have_css('#p2_health', :text => '90')
   end
+
 end
