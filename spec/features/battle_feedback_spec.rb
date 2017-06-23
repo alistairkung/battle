510ea9ae28f54328a0e93cc8630a3a26adf13ec8
Alistair Kung <alistaircckung@gmail.com>
2017-06-23 01:25:27 +0100

    extracted battle feedback tests into new spec

---
+++ spec/features/battle_feedback_spec.rb
@@ -0,0 +1,17 @@
+
+feature "battle feedback" do
+  scenario "player one clicks attack button" do
+    sign_in_and_play
+    click_on 'Attack'
+    expect(page).to have_content("player2's turn to attack")
+    expect(page).to have_content('player attacked player2!')
+  end
+
+  scenario "player two clicks attack button" do
+    sign_in_and_play
+    click_on 'Attack'
+    click_on 'Attack'
+    expect(page).to have_content("player's turn to attack")
+    expect(page).to have_content('player2 attacked player!')
+  end
+end
